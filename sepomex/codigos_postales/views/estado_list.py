ed5d004e7597c3da3527d85d5f2ea25a61b0f5a0
cupc4ke <miguel.martinez@ciencias.unam.mx>
2021-04-09 11:24:24 -0500

    get api view de listado de estados

---
+++ sepomex/codigos_postales/views/estado_list.py
@@ -0,0 +1,61 @@
+# utils
+from django.core.paginator import Paginator
+from sepomex.serializers.paginator_serializer import PaginatorSerializer
+
+# modelos y serializadores de estado
+from codigos_postales.models.estado import Estado
+from codigos_postales.serializers.serializador_estado import SerializadorEstado
+
+# manejo de autenticacion y respuesta
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+# este se usaría si se implementa post, patch, put o delete 
+# from rest_framework import status
+
+class EstadoList(APIView):
+    """
+    View para listar los estados
+    """
+    # descomentar la siguiente línea cuando se tenga implementado el sistema de usuarios 
+    # permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+        # obtenemos la informacion de la paginación, en caso de no ser provista, respondemos con la información completa, dado que sólo son 32 estados.
+        # TODO: discutir query string required o no
+        page_number = int(request.GET['page_number']) if 'page_number' in request.GET else 1
+        page_size = int(request.GET['page_size']) if 'page_size' in request.GET else 32
+        
+        # el queryset consta de todos los estados
+        estados = Estado.objects.all()
+
+        # creamos paginator con la cantidad de estados requerida
+        paginator = Paginator(
+            object_list=estados,
+            per_page=page_size, 
+        )
+
+        # usando el paginator, serializamos los estados requeridos junto a la informacion de paginacion
+        serializer = SerializadorEstado(
+            paginator.page(number=page_number).object_list,
+            many=True
+        )
+        pagination_info = PaginatorSerializer(
+            paginator=paginator,
+            page_number=page_number,
+        )
+        return Response(
+            {
+                'data': serializer.data,
+                'pagination_info': pagination_info.to_dict()
+            }
+        )
+
+    # A continuación, un ejemplo de como implementar el método post en caso que así se quisiera.
+    # def post(self, request, format=None):
+    #     serializer = SerializadorEstado(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response({'estado': serializer.data}, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors)
