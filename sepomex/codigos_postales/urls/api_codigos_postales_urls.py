09133fca1beea2208bafa4cd116207993f1d2252
cupc4ke <miguel.martinez@ciencias.unam.mx>
2021-04-09 13:57:50 -0500

    urls de list de codigos postales y asentamientos

--- sepomex/codigos_postales/urls/api_codigos_postales_urls.py
+++ sepomex/codigos_postales/urls/api_codigos_postales_urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 # imports de vistas
 from codigos_postales.views.estado_list import EstadoList
 from codigos_postales.views.municipio_list import MunicipioList
+from codigos_postales.views.asentamiento_list import AsentamientoList
+from codigos_postales.views.codigo_postal_list import CodigoPostalList
 
 urlpatterns = [
 
@@ -17,6 +19,18 @@ urlpatterns = [
         'municipios',
         view=MunicipioList.as_view(),
         name='municipios_list_view',
+    ),
+
+    path(
+        'asentamientos',
+        view=AsentamientoList.as_view(),
+        name='asentamientos_list_view',
+    ),
+
+    path(
+        'codigos_postales',
+        view=CodigoPostalList.as_view(),
+        name='codigos_postales_list_view',
     ),
 
 ]
