1d7692ead3b53d83ea28bc97dd8b4f1852054d91
cupc4ke <miguel.martinez@ciencias.unam.mx>
2021-04-09 10:46:10 -0500

    se agregan modelos al admin para obligar migraciones

--- sepomex/codigos_postales/admin.py
+++ sepomex/codigos_postales/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from codigos_postales.models.estado import Estado
+from codigos_postales.models.municipio import Municipio
+from codigos_postales.models.asentamiento import Asentamiento
+from codigos_postales.models.codigo_postal import CodigoPostal
 
-# Register your models here.
+# class EstadoAdmin(admin.ModelAdmin):
+#     list_display = ('estado', 'fecha_ultima_actualizacion')
+#     list_filter = ('fecha_ultima_actualizacion')
+
+
+
+
+admin.site.register(Estado)
+admin.site.register(Municipio)
+admin.site.register(Asentamiento)
+admin.site.register(CodigoPostal)
